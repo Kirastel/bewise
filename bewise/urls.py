ee8088d0c2082b0f54ac3896817725bededbf79e
Kirastel <97628188+Kirastel@users.noreply.github.com>
2022-05-06 21:33:27 +0300

    Update urls.py

--- bewise/urls.py
+++ bewise/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main_app.views import QuestionView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/question/', QuestionView.as_view()),
 ]
